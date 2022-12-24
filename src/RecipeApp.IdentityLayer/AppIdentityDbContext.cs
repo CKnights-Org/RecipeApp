bff25d53456f0fa0c5c8bedc8b60493822c30098
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2022-12-24 04:29:52 +0100

    Added missing requirement for multiple db contexts

--- src/RecipeApp.IdentityLayer/AppIdentityDbContext.cs
+++ src/RecipeApp.IdentityLayer/AppIdentityDbContext.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace RecipeApp.IdentityLayer
 {
+    //create migrations with 
+    //CLI: dotnet ef migrations add SomeName -s ../RecipeAppMVC/RecipeAppMVC.csproj -c AppIdentityDbContext
+    //VS: Add-Migration SomeName -Context AppIdentityDbContext
     public class AppIdentityDbContext : IdentityDbContext
     {
         public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
