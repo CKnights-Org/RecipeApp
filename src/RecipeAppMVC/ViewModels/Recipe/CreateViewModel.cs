f063420cb2b661aeb087f314f0e7cb9e43006b87
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2022-12-27 06:51:44 +0100

    added VM logic for adding ingredients

--- src/RecipeAppMVC/ViewModels/Recipe/CreateViewModel.cs
+++ src/RecipeAppMVC/ViewModels/Recipe/CreateViewModel.cs
@@ -1,12 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RecipeAppDAL.Models;
 
 namespace RecipeAppMVC.ViewModels.Recipe
 {
     public class CreateViewModel
     {
-        
+        [BindProperty, Required]
+        public string Name { get; set; } = "";
+        [BindProperty, Required]
+        public string Description { get; set; } = "";
+        [BindProperty, Required]
+        public IList<IngredientRecipe> Ingredients { get; set; } = new List<IngredientRecipe>();
+        [NotMapped]
+        public IngredientRecipe? NewIngredient { get; set; }
+        [NotMapped]
+        public IEnumerable<SelectListItem> IngredientsSelection { get; set; } = null!;
     }
 }
