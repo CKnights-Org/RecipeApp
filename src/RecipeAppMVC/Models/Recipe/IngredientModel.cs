2b6a17e77b327657ca02336b1561457ef4bd3c8a
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2023-01-21 04:38:43 +0100

    Made Amount and amount type required

--- src/RecipeAppMVC/Models/Recipe/IngredientModel.cs
+++ src/RecipeAppMVC/Models/Recipe/IngredientModel.cs
@@ -1,13 +1,16 @@
-﻿namespace RecipeAppMVC.Models.Recipe
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeAppMVC.Models.Recipe
 {
     public class IngredientModel
     {
         public int Id { get; set; }
 
         public string? Name { get; set; }
-
+        
+        [Required]
         public int Amount { get; set; }
-
+        [Required]
         public string? TypeOfAmount { get; set; }
     }
 }
